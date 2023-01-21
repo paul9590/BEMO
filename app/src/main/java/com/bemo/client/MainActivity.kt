a562d9068d507e3dbd48ab73b55c5b80acaa3c59
paul9590 <paul95901234@gmail.com>
2023-01-21 15:57:10 +0900

    add splash image

--- app/src/main/java/com/bemo/client/MainActivity.kt
+++ app/src/main/java/com/bemo/client/MainActivity.kt
@@ -2,10 +2,25 @@ package com.bemo.client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.bemo.client.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val mBinding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mBinding.root)
+
+        mBinding.root.viewTreeObserver.addOnPreDrawListener (
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // TODO: splash 화면
+                    return false
+                }
+            }
+        )
     }
 }
