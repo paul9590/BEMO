41d4798fe18aca26852d60f37b1a4492ca466edb
paul9590 <paul95901234@gmail.com>
2023-01-29 03:37:24 +0900

    add CompanyActivity.kt

---
+++ app/src/main/java/com/bemo/client/activity/CompanyActivity.kt
@@ -0,0 +1,21 @@
+package com.bemo.client.activity
+
+import android.graphics.PorterDuff
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bemo.client.R
+import com.bemo.client.databinding.ActivityCompanyBinding
+
+class CompanyActivity: AppCompatActivity() {
+
+    private val mBinding: ActivityCompanyBinding by lazy { ActivityCompanyBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(mBinding.root)
+        val company = intent.getStringExtra("company")
+        mBinding.apply {
+            txtCompanyName.text = company
+        }
+    }
+}
--- app/src/main/java/com/bemo/client/recycler/CompanyRecyclerAdapter.kt
+++ app/src/main/java/com/bemo/client/recycler/CompanyRecyclerAdapter.kt
@@ -1,9 +1,11 @@
 package com.bemo.client.recycler
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bemo.client.activity.CompanyActivity
 import com.bemo.client.databinding.ListCompanyBinding
 import com.bemo.client.model.Company
 
@@ -20,6 +22,9 @@ class CompanyRecyclerAdapter : RecyclerView.Adapter<CompanyRecyclerAdapter.Compa
         holder.bind(companyList[position])
         holder.mBinding.root.setOnClickListener {
             //TODO company details
+            val intent = Intent(view.context, CompanyActivity::class.java)
+            intent.putExtra("company", "송수학 학원")
+            view.context.startActivity(intent)
         }
     }
 
